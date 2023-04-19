b7d93cefbea58ac1ba1be5c4b28b97ed802f2d1d
Jesus Mejia <jesusmejded@gmail.com>
2023-04-19 12:03:19 -0600

    verbose results

--- src/external.jl
+++ src/external.jl
@@ -5,7 +5,7 @@ import Metaheuristics: initialize!, update_state!, final_stage!, stop_criteria!
 import Metaheuristics: optimize, is_better
 import Random: seed!, randperm
 import Base: show, minimum
-import Printf: @printf
+import Printf: @printf, @sprintf
 import Statistics: std, mean, var
 import Optim
 import LinearAlgebra: norm
